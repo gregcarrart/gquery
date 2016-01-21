ce741dea58cdd8ff39b769843940f0a4955205f6
Greg Carr <gregcarrart@gmail.com>
2016-01-20 16:37:19 -0800

    1.20.16 updates

--- src/gquery.js
+++ src/gquery.js
@@ -15,34 +15,174 @@
     gQuery.fn = gQuery.prototype = {
         init: function (selector) {
             var element = document.querySelectorAll(selector) || [];
-            if (element.length > 1) {
-                this.selector = element;
-            } else {
-                this.selector = element[0];
+            var about = {
+                Version: 2.0,
+                Author: 'gregcarrart',
+                Created: '9.19.2015',
+                Updated: '1.20.2016'
             }
+            if (selector) {
+                if (element.length > 1) {
+                    this.selector = element;
+                } else {
+                    this.selector = element[0];
+                }
 
-            return this;
+                this.length = element.length;
+
+                if (window === this) {
+                    return new $(selector);
+                }
+                return this;
+            } else {
+                return about;
+            }
         },
-        css: function (object) {
+        css: function (property, style) {
             var propTypes = {
                 'background-image': 'backgroundImage',
-                'background-position': 'backgroundPosition'
+                'background-position': 'backgroundPosition',
+                'background-color': 'backgroundColor',
+                'background-size': 'backgroundSize',
+                'background-repeat': 'backgroundRepeat',
+                'background-attachment': 'backgroundAttachment',
+                'background-origin': 'backgroundOrigin'
             }
-
-            for (var key in object) {
-                if (object.hasOwnProperty(key)) {
-                    var value = object[key];
-                    if (propTypes.hasOwnProperty(key)) {
-                        this.selector.style[propTypes[key]] = value;
+            if (this.selector.length > 1) {
+                for (var i = 0; i < this.selector.length; i++) {
+                    if (typeof property === 'object' && typeof style === 'undefined') {
+                        for (var key in property) {
+                            if (property.hasOwnProperty(key)) {
+                                var value = property[key];
+                                if (propTypes.hasOwnProperty(key)) {
+                                    this.selector[i].style[propTypes[key]] = value;
+                                } else {
+                                    this.selector[i].style[key] = value;
+                                }
+                            }
+                        }
+                    } else if (typeof property === 'string' && typeof style === 'string') {
+                        if (propTypes.hasOwnProperty(property)) {
+                            this.selector[i].style[propTypes[property]] = style;
+                        } else {
+                            this.selector[i].style[property] = style;
+                        }
+                    } else if (typeof property === 'string' && typeof style === 'undefined') {
+                        var style = window.getComputedStyle(this.selector[i]);
+                        return style.getPropertyValue(property);
                     } else {
-                        this.selector.style[key] = value;
+                        return window.getComputedStyle(this.selector[i]);
                     }
+                }
+            } else {
+                if (typeof property === 'object' && typeof style === 'undefined') {
+                    for (var key in property) {
+                        if (property.hasOwnProperty(key)) {
+                            var value = property[key];
+                            if (propTypes.hasOwnProperty(key)) {
+                                this.selector.style[propTypes[key]] = value;
+                            } else {
+                                this.selector.style[key] = value;
+                            }
+                        }
+                    }
+                } else if (typeof property === 'string' && typeof style === 'string') {
+                    if (propTypes.hasOwnProperty(property)) {
+                        this.selector.style[propTypes[property]] = style;
+                    } else {
+                        this.selector.style[property] = style;
+                    }
+                } else if (typeof property === 'string' && typeof style === 'undefined') {
+                    var style = window.getComputedStyle(this.selector);
+                    return style.getPropertyValue(property);
+                } else {
+                    return window.getComputedStyle(this.selector);
                 }
             }
         },
         on: function (eventType, callback) {
             this.selector.addEventListener(eventType, callback, false);
+        },
+        map: function (callback) {
+            var results = [], i = 0;
+            for ( ; i < this.length; i++) {
+                results.push(callback.call(this, this[i], i));
+            }
+            return results;
+        },
+        mapOne: function (callback) {
+            var m = this.map(callback);
+            return m.length > 1 ? m : m[0];
+        },
+        forEach: function (callback) {
+            this.map(callback);
             return this;
+        },
+        text: function (text) {
+            if (typeof text !== "undefined") {
+                return this.forEach(function () {
+                    this.selector.innerText = text;
+                });
+            } else {
+                return this.mapOne(function () {
+                    return this.selector.innerText;
+                });
+            }
+        },
+        html: function (html) {
+            if (typeof html !== "undefined") {
+                this.forEach(function () {
+                    this.selector.innerHTML = html;
+                });
+                return this;
+            } else {
+                return this.mapOne(function () {
+                    return this.selector.innerHTML;
+                });
+            }
+        },
+        addClass: function (classes) {
+            var className = '';
+            var el = this.selector;
+
+            if (typeof classes !== 'string') {
+                for (var i = 0; i < classes.length; i++) {
+                    className += ' ' + classes[i];
+                }
+            } else {
+                className = ' ' + classes;
+            }
+
+            return this.forEach(function () {
+                this.selector.className += className;
+            });
+        },
+        removeClass: function (cl) {
+            return this.forEach(function () {
+                var cs = this.selector.className.split(' '), i;
+
+                while ( (i = cs.indexOf(cl)) > -1) {
+                    cs = cs.slice(0, i).concat(cs.slice(++i));
+                }
+                this.selector.className = cs.join(' ');
+            });
+        },
+        attr: function (attr, val) {
+            if (typeof val !== 'undefined') {
+                return this.forEach(function () {
+                    this.selector.setAttribute(attr, val);
+                });
+            } else {
+                return this.mapOne(function () {
+                    return this.selector.getAttribute(attr);
+                });
+            }
+        },
+        hide: function() {
+            this.selector.style['display'] = 'none';
+        },
+        show: function() {
+            this.selector.style['display'] = 'block';
         }
     };
 
